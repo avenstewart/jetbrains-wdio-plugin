14b20d8c8972db415566b2bb4d43b4ec3a41cf33
Aven Stewart <aven@perryweather.com>
2026-05-07 15:03:35 -0500

    teach WdioTestFrameworkDetector to expose JS test structure for the gutter contributor

--- src/main/kotlin/com/perryweather/wdio/framework/WdioTestFrameworkDetector.kt
+++ src/main/kotlin/com/perryweather/wdio/framework/WdioTestFrameworkDetector.kt
@@ -3,10 +3,20 @@
 
 package com.perryweather.wdio.framework
 
+import com.intellij.javascript.testFramework.AbstractTestFileStructure
+import com.intellij.javascript.testFramework.interfaces.mochaTdd.MochaTddFileStructureBuilder
+import com.intellij.javascript.testFramework.jasmine.JasmineFileStructureBuilder
 import com.intellij.javascript.testing.detection.JsTestFrameworkApiDesign
 import com.intellij.javascript.testing.detection.JsTestFrameworkDetector
+import com.intellij.lang.javascript.psi.JSFile
 
 class WdioTestFrameworkDetector : JsTestFrameworkDetector {
     override val frameworkName: String = "WebdriverIO"
     override val frameworkApiDesign: JsTestFrameworkApiDesign = JsTestFrameworkApiDesign.GLOBAL_VARIABLES
+
+    override fun findTestsStructure(jsFile: JSFile): AbstractTestFileStructure {
+        val jasmine = JasmineFileStructureBuilder.getInstance().fetchCachedTestFileStructure(jsFile)
+        if (!jasmine.isEmpty) return jasmine
+        return MochaTddFileStructureBuilder.getInstance().fetchCachedTestFileStructure(jsFile)
+    }
 }
